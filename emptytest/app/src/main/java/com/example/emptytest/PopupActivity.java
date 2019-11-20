75b454eaa12b28dbbc8ff0270910a412b6a7e430
rowflo <rowflo@gmail.com>
2019-11-20 01:25:24 -0800

    Add files via upload

---
+++ emptytest/app/src/main/java/com/example/emptytest/PopupActivity.java
@@ -0,0 +1,14 @@
+package com.example.emptytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup);
+    }
+}
